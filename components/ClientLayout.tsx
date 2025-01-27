6c9275bf8ac169fff2ec41a37d4767725829398e
benjamenharper <benjamen.harper@gmail.com>
2025-01-27 04:12:17 -1000

    Move StoreProvider to dedicated providers directory

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -3,7 +3,7 @@
 import { ThemeProvider } from 'next-themes';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { StoreProvider } from '@/lib/store';
+import { StoreProvider } from '@/components/providers/StoreProvider';
 
 export default function ClientLayout({
   children,
