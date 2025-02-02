565a0f6f1df62e50c7f5271a8a23bd620c3318dc
benjamenharper <benjamen.harper@gmail.com>
2025-02-02 07:34:56 -1000

    feat: update logo path in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
           <div className="flex items-center">
             <Link href="/" className="flex items-center gap-4">
               <Image
-                src="/malogo2.png"
+                src="/musicadvisorlogonew.png"
                 alt="MusicAdvisor Logo"
                 width={48}
                 height={48}
